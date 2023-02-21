382af793f835c40822550990652f905031816780
Andrey Bezkorovayniy <duhasmok@mail.ru>
2023-02-21 19:38:12 +0300

    Первая версия игры калькулятор

--- brain_games_1/scripts/brain_calc.py
+++ brain_games_1/scripts/brain_calc.py
@@ -1,5 +1,6 @@
 import random
 import operator
+import prompt
 from brain_games_1 import cli
 
 
@@ -16,10 +17,19 @@ def game_calc():
         "*": operator.mul,
     }
     for _ in range(3):
-        quest = action[](random.randint(1, 100), random.randint(1, 100))
-        print(f'Question: {quest}')
-
-
+        a, b = random.randint(1, 50), random.randint(1, 20)
+        oper = random.choice(list(action))
+        quest = action[oper](a, b)
+        print(f'Question: {a} {oper} {b}')
+        answer_player = prompt.string('Your answer: ')
+        if int(answer_player) == quest:
+            print('Correct!')
+        else:
+            print(f"{answer_player} is wrong answer ;(. Correct answer was {quest}.")
+            print(f"Let's try again, {cli.name}!")
+            break
+    else:
+        print(f'Congratulations, {cli.name}!')
 
 
 if __name__ == '__main__':
