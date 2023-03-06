8439c0eb38f83ce97f8469e50a92aa0c0e0a29ef
Andrey Bezkorovayniy <duhasmok@mail.ru>
2023-03-06 16:54:04 +0300

    Изменение логики скрипта
    Весь код вынесен в отдельный модуль

--- brain_games_1/scripts/brain_calc.py
+++ brain_games_1/scripts/brain_calc.py
@@ -1,36 +1,14 @@
-import random
-import operator
-import prompt
-from brain_games_1 import cli
+#!/usr/bin/env python
+
+
+from brain_games_1.logic import logic_game
+from brain_games_1.game import game_calc
 
 
 def main():
-    cli.welcome_user()
-    print('What is the result of the expression?')
-    game_calc()
-
-
-def game_calc():
-    action = {
-        "+": operator.add,
-        "-": operator.sub,
-        "*": operator.mul,
-    }
-    for _ in range(3):
-        a, b = random.randint(1, 50), random.randint(1, 20)
-        oper = random.choice(list(action))
-        quest = action[oper](a, b)
-        print(f'Question: {a} {oper} {b}')
-        answer_player = prompt.integer('Your answer: ')
-        if int(answer_player) == quest:
-            print('Correct!')
-        else:
-            print(f"{answer_player} is wrong answer ;(. Correct answer was {quest}.")
-            print(f"Let's try again, {cli.name}!")
-            break
-    else:
-        print(f'Congratulations, {cli.name}!')
+    logic_game(game_calc)
 
 
 if __name__ == '__main__':
     main()
+
