ff0681ba2670ef17e350a77d4c4a801cef7f7396
Andrey Bezkorovayniy <duhasmok@mail.ru>
2023-03-09 18:28:39 +0300

    Добавление последней версии progression

--- brain_games_1/game/game_progression.py
+++ brain_games_1/game/game_progression.py
@@ -1,1 +1,21 @@
+import random
+
+
 RULES = 'What number is missing in the progression?'
+
+
+def get_game():
+    length = random.randint(5, 10)
+    start = random.randint(0, 50)
+    step = random.randint(1, 10)
+    data = list()
+    data.append(start)
+
+    for i in range(length-1):
+        data.append(data[i]+step)
+    index = random.randint(0, length-1)
+    data = [str(x) for x in data]
+    answer = data[index]
+    data[index] = '..'
+    quest = ' '.join(data)
+    return answer, quest
