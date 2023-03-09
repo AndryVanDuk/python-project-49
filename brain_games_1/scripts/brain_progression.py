013846eb841bd91364b71c2d1563877a4af97c0b
Andrey Bezkorovayniy <duhasmok@mail.ru>
2023-03-09 18:00:36 +0300

    Скрипт для игры progression

---
+++ brain_games_1/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from brain_games_1.logic import logic_game
+from brain_games_1.game import game_progression
+
+
+def main():
+    logic_game(game_progression)
+
+
+if __name__ == '__main__':
+    main()
